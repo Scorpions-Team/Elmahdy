12e1ab5bed786752528f4066d029af24cdbff22e
Youssef Ahmed <ya23122002@gmail.com>
2023-08-24 01:13:00 +0300

    u

--- components/Layout.js
+++ components/Layout.js
@@ -7,7 +7,7 @@ const Layout = ({ children }) => {
     <>
       <Head>
         <title>المهدي للمأكولات البحرية</title>
-        <meta name="description" content="Elmahdy for all types of seafood" />
+        <meta name="description" content="المهدي لجميع المأكولات البحرية" />
         <meta
           name="google-site-verification"
           content="OMH7wWSmsl7zK8fchGabFF0v5n4NVUoq7vkhYPNmr7Q"
